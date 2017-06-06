0b0d4e8672d8e4aba918b4b7f30f04c94b54de45
Kiran Karpurapu <kvk229@nyu.edu>
2017-06-05 21:46:51 -0400

    added scroll view for time picker fragment

---
+++ app/src/main/java/com/example/kirank/yogatimer/ui/SpecialStart.java
@@ -0,0 +1,15 @@
+package com.example.kirank.yogatimer.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.kirank.yogatimer.R;
+
+public class SpecialStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_special_start);
+    }
+}
